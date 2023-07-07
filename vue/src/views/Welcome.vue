dd7bf2ac854791eebd8619eef23a9e89171271f6
Hanindriyo76 <hanindriyo@students.amikom.ac.id>
2023-07-07 22:51:01 +0700

    foto

--- vue/src/views/Welcome.vue
+++ vue/src/views/Welcome.vue
@@ -414,9 +414,9 @@ const people = [
     },
     {
         name: "Lucky Hanindriyo_4313",
-        role: "-",
+        role: "Tester",
         imageUrl:
-            "https://img.freepik.com/free-photo/orangutang-chilling-out-hammock_181624-45402.jpg?w=826&t=st=1688025295~exp=1688025895~hmac=c8d560db1b3f528a6bb1e210273d804e868ec3d91e751c92d77bc3765bded598",
+            "public/images/Lucky.jpg",
     },
     {
         name: "Muhammad Taqiyuddin_4328",
